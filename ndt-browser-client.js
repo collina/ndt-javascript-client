collina/ndt-javascript-client

commit 6b690b8ecc7f35ad64f9314f1701ceae6aa3b2d8

Simplify the for loop a bit.

ndt-browser-client.js
@@ -59,8 +59,8 @@ function make_ndt_msg(type, msg) {
     NDT_MSG[0] = type;
     NDT_MSG[1] = (message_body.length >> 8) & 0xFF;
     NDT_MSG[2] = message_body.length & 0xFF;
-    for (i = 0; i < message_body.length; i += 1) {
-        NDT_MSG[i + 3] = message_body.charCodeAt(i);
+    for (i = 3; i < message_body.length; i++) {
+        NDT_MSG[i] = message_body.charCodeAt(i);
     }
     return NDT_MSG;
 }

commit 391cef4cd306ba190507aef4d164d393095bb194

Removed a bug I introduced in 6b690b8... guess it wasn't a simplification after all.

index.html
@@ -15,7 +15,7 @@
 
 <script>
     var server = "ndt.iupui.mlab2.nuq0t.measurement-lab.org";
-    var server_port = "7000";
+    var server_port = "8888";
 	var url = "ws://" + server + ":" + server_port + "/ndt_after_user_privacy_agreement";
 
 	document.getElementById('start_test').addEventListener('click', function() {

ndt-browser-client.js
@@ -58,8 +58,8 @@ function make_ndt_msg(type, msg) {
 	NDT_MSG[0] = type;
 	NDT_MSG[1] = (message_body.length >> 8) & 0xFF;
 	NDT_MSG[2] = message_body.length & 0xFF;
-	for (i = 3; i < message_body.length; i++) {
-		NDT_MSG[i] = message_body.charCodeAt(i);
+	for (i = 0; i < message_body.length; i++) {
+		NDT_MSG[i + 3] = message_body.charCodeAt(i);
 	}
 	return NDT_MSG;
 }
@@ -130,7 +130,7 @@ function ndt_s2c_test(sock) {
 	return function (type, body) {
 		var TEST_DURATION_SECONDS,
 			THROUGHPUT_VALUE;
-		logMessage("CALLED S2C with " + type + " " + msg_name[type] + " " + body.msg + " in state " + state);
+		logMessage("CALLED S2C with " + type + " " + msg_name[type] + " in state " + state);
 		if (state === "WAIT_FOR_TEST_PREPARE" && type === TEST_PREPARE) {
 			server_port = Number(body.msg);
 			// bind a connection to that port